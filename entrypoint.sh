6c089cfb88d39925d5fbb09e4f4a725b5d34e6f2
Shane Arcaro <smarcaro@gmail.com>
2023-12-10 12:55:56 -0500

    I'm starting to figure this out

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,30 @@
 #!/bin/sh
 
+# Read in path and languages to traverse
 paths=$1
 languages=$2
+event_type=$3
 
-echo "Paths: ${paths}"
-echo "Languages: ${languages}"
+echo "Paths: ${paths} \n"
+echo "Languages: ${languages} \n"
 
-index=-1
-for lang in ${languages}; do
-  echo "Extension: $lang"
-  if [ $index -gt -1 ]; then
-    echo "Language: ${languages[$index]}"
+# Add labels to events
+add_labels() {
+  if [ "$event_type" = "pull_request" ]; then
+    gh pr edit --add-label "$1"
+  else
+    gh issue edit --add-label "$1"
   fi
-  echo "Index: $index"
-  index=$((index + 1))
+}
+
+extension=""
+for item in $languages; do
+  # Check if item has a trailing colon
+  if echo "$item" | grep -q ':$'; then
+    # Remove trailing colon
+    extension=${item%:*}
+  else
+    echo "Label: $item Extension: $extension"
+    add_labels "$item"
+  fi
 done
