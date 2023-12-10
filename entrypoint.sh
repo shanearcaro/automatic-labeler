832abead5a401f9466b1f5993936cce7b0b2636e
Shane Arcaro <smarcaro@gmail.com>
2023-12-10 11:46:37 -0500

    Updating entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,12 @@ languages=$2
 echo "Paths: ${paths}"
 echo "Languages: ${languages}"
 
+index=-1
 for lang in ${languages}; do
   echo "$lang"
+  if [ $index -g -1 ]; then
+    echo "${lang[$index]}"
+  fi
+  ((index++))
+
 done
