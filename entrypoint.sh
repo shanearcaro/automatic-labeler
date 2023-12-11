606eb63b4e8e6b6c6b1aad78ce451b465bdbd89c
Shane Arcaro <smarcaro@gmail.com>
2023-12-11 10:14:43 -0500

    Messing with script

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,20 +7,22 @@ git config --global --add safe.directory $PWD
 paths=$1
 languages=$2
 
+# Define aliases
 head=$GITHUB_HEAD_REF
 base=$GITHUB_BASE_REF
-event=$INPUT_EVENT
+git_event=$INPUT_EVENT
 
 echo "Head: ${head}"
 echo "Base: ${base}"
-echo "Event: ${event}"
+echo "Event: ${git_event}"
 
 echo "Paths: ${paths} \n"
 echo "Languages: ${languages} \n"
 
 # Add labels to events
 add_labels() {
-  if [ $event = "pull_request" ]; then
+  echo "Value $1"
+  if [ $git_event = "pull_request" ]; then
     gh pr edit --add-label "$1"
   else
     gh issue edit --add-label "$1"
@@ -29,8 +31,9 @@ add_labels() {
 
 # Search for a label
 search_label() {
-  search=$(gh label list -S "$1")
-  echo "$search"
+  label=$(echo $1 | xargs)
+  search=$(gh label list -S $label)
+  echo $search
 }
 
 create_label() {
@@ -40,22 +43,55 @@ create_label() {
 }
 
 add_pr_labels() {
-  files=$(git diff --name-only "$head" "$base")
   tags_added=""
+  echo "Is this working: $1"
 
-  for file in $files; do
-    # Extract file extension5
+  for file in $1; do
+    # Extract file extensions
     file_ext="${file##*.}"
 
     # Check if file extension is in languages
     found_ext=$(echo "$languages" | grep -o "$file_ext")
     if [ -n "$found_ext" ]; then
       if [ -z $(echo "$tags_added" | grep -o "$file_ext") ]; then
-        add_labels
-        tags_added="$tags_added $file_ext"
-        echo "$file_ext tag added"
+        add_labels $2
+        echo "$2 tag added"
       else
-        echo "$file_ext tag already added"
+        echo "$2 tag already added"
+      fi
+    fi
+  done
+}
+
+get_changed_files() {
+  changed_files=""
+  if [ $git_event = "pull_request" ]; then
+    changed_files=$(git diff --name-only "$head" "$base")
+  else
+    changed_files=$(git diff --name-only "$head")
+  fi
+  echo $changed_files
+}
+
+generate_labels() {
+  changed_files=$(get_changed_files)
+  echo "Changed files: $changed_files"
+  extensions=""
+
+  for item in $languages; do
+    # Check if item has a trailing colon
+    if echo "$item" | grep -q ':$'; then
+      # Remove trailing colon
+      extension=${item%:*}
+    else
+      echo "Label: $item Extension: $extension"
+      search_result=$(search_label $item)
+      if [ -z "$search_result" ]; then
+        echo "Label not found, creating label"
+        create_label "$item"
+      else
+        echo "Label found, adding label"
+        add_pr_labels "$changed_files" "$item"
       fi
     fi
   done
@@ -63,25 +99,5 @@ add_pr_labels() {
 
 # Need to checkout branch before using git commands
 gh pr checkout "$head"
+generate_labels
 
-add_pr_labels
-
-
-# extension=""
-# for item in $languages; do
-#   # Check if item has a trailing colon
-#   if echo "$item" | grep -q ':$'; then
-#     # Remove trailing colon
-#     extension=${item%:*}
-#   else
-#     echo "Label: $item Extension: $extension"
-#     echo "Searching for label: $item"
-#     if [ -z "$(search_label "$item")" ]; then
-#       echo "Label not found, creating label"
-#       create_label "$item"
-#     else
-#       echo "Label found, adding label"
-#       add_labels "$item"
-#     fi
-#   fi
-# done
